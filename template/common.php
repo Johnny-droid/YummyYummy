cac4899abcad72cad886579626bc7be53a3f71f0
Zé Araújo <72151399+zearaujo11@users.noreply.github.com>
2022-04-29 14:43:51 +0100

    adding some files

---
+++ template/common.php
@@ -0,0 +1,20 @@
+<?php function output_header() { ?>
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>YummiYummi</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/layout.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <header>
+            <h1><a href="index.php">HOME</a></h1>
+            <h1><a href="
+        </header>
+    </body>
+
+</html>
+<?php } ?>
