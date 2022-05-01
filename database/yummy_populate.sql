09114a3d1e91d2022120539ad37f5ea6b2869efd
zearaujo11 <72151399+zearaujo11@users.noreply.github.com>
2022-05-01 16:03:34 +0100

    adding couple of restaurants

---
+++ database/yummy_populate.sql
@@ -0,0 +1,12 @@
+PRAGMA foreign_keys = on; 
+
+insert into Restaurant values(1, 'Grelha do Manuel', 932659956, Porto, 8:00:00, 23:00:00, 1); 
+insert into Restaurant values(2, 'Hamburgueres do Carlos', 932828456, Porto, 8:30:00, 23:00:00, 2);
+insert into Restaurant values(3, 'Grelha do Aníbal', 937468123, Porto, 9:00:00, 23:00:00, 3);
+insert into Restaurant values(4, 'Restaurante do Pedro', 912345723, Porto, 8:00:00, 24:00:00, 4);
+insert into Restaurant values(5, 'Roulote da Vânia', 928754321, Porto, 8:00:00, 23:00:00, 5);
+insert into Restaurant values(6, 'Pizzaria Vasco', 910045769, Porto, 8:00:00, 23:00:00, 6);
+insert into Restaurant values(7, 'Sushiaria', 923465782, Porto, 8:00:00, 23:00:00, 7);
+insert into Restaurant values(8, 'Xi-Yuan', 933325648, Porto, 8:00:00, 23:00:00, 8);
+
+
