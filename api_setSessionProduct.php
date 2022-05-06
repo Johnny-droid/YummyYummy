9d7a81545303ea554e3684c83a784b1646745827
Johnny-droid <joaoricardoralves@gmail.com>
2022-05-06 18:13:23 +0100

    Now, when the page is reloaded, the order is still shown

--- api_setSessionProduct.php
+++ api_setSessionProduct.php
@@ -5,13 +5,15 @@
 
     //$_SESSION['products'] = array();
 
-    if (isset($_SESSION['products']) && !empty($_SESSION['products'])) {
-        $_SESSION['products'][] = $_GET['id_product'];
+    if (isset($_SESSION['products']) && !empty($_SESSION['products']) && $_GET['id_product']) {
+        array_push($_SESSION['products'], $_GET['id_product']);
+        echo json_encode($_SESSION['products']);
+    } else if (isset($_SESSION['products']) && !$_GET['id_product']) {
+        echo json_encode($_SESSION['products']);
     } else {
         $_SESSION['products'] = array($_GET['id_product']);
+        echo json_encode($_SESSION['products']);
     }
 
-
-    echo json_encode($_SESSION['products']);
     
 ?>
--- javascript/makeOrder.js
+++ javascript/makeOrder.js
@@ -1,82 +1,98 @@
+async function showProductInOrder(id_product, addToSession) {
+    if (addToSession) {
+        const response = await fetch('api_setSessionProduct.php?id_product=' + id_product);
+        const session = await response.json() 
+    } 
+    
+    
+    const aside = document.querySelector('.clientOrders');
+    if (!aside) {
+        aside.innerHTML = '';
+        const h1Order = document.createElement('h1')
+        h1Order.innerHTML = 'Order'
+        aside.appendChild(h1Order)
+        aside.classList.add('clientOrders')
+    }
+    
+    const productDiv = document.getElementById('productDiv' + id_product)
 
+    if (!productDiv) {
+
+        const responseProduct = await fetch('api_getProduct.php?id_product=' + id_product);
+        const product = await responseProduct.json();
+        
+        const productDivNew = document.createElement('div')
+        const productName = document.createElement('strong')
+        const productPrice = document.createElement('em')
+        const productQuantity = document.createElement('em')
+        const productRemove = document.createElement('button')
+
+        const price = (product.price * (1 - (product.discount/100)));
+        productPrice.innerHTML = (price.toFixed(2))  + ' € ';
+        productName.innerHTML = product.name
+        productQuantity.innerHTML = 1;
+        productQuantity.id = "productQuantity" + id_product;
+        productRemove.innerHTML = 'Remove'
+        productRemove.value = id_product
+        productRemove.addEventListener('click', async function() {
+            await fetch('api_removeSessionProduct.php?id_product=' + id_product);
+            const productDivToRemove = document.getElementById('productDiv' + id_product);
+            if (productDivToRemove) {
+                productDivToRemove.remove();
+            }
+        })
+        productRemove.classList.add('productOrderRemoveButton')
+
+        productDivNew.classList.add('productInOrders')
+        productDivNew.id = 'productDiv' + id_product
+        productDivNew.appendChild(productName)
+        productDivNew.appendChild(productPrice)
+        productDivNew.innerHTML += " Quantity: "
+        productDivNew.appendChild(productQuantity)
+        productDivNew.appendChild(productRemove)
+        aside.appendChild(productDivNew)
+    
+    } else {
+        const productQuantityId = document.querySelector("#productQuantity" + id_product)
+        if (productQuantityId) {
+            productQuantityId.innerHTML = parseInt(productQuantityId.innerHTML) + 1;
+        }
+            
+    }
+    
+}
+
+async function showProductInOrderBeginning() {
+    const response = await fetch('api_setSessionProduct.php')
+    const sessionProductsObject = await response.json();
+    let sessionProducts = Object.values(sessionProductsObject)
+    sessionProducts = sessionProducts.filter(function(el) {return el !== null})
+
+    sessionProducts.forEach( function (product_id) {
+        if (product_id) {
+           showProductInOrder(product_id, false) 
+        }
+    }) 
+}
+
+showProductInOrderBeginning();
 
 const addProductsToOrderButtons = document.querySelectorAll('.productOrderAddButton')
 
 if (addProductsToOrderButtons) {
-    addProductsToOrderButtons.forEach(function(addProductsToOrderButton) {
-        addProductsToOrderButton.addEventListener('click', async function() {
-            const responseSession = await fetch('api_setSessionProduct.php?id_product=' + this.value);
-            const productsIds = await responseSession.json();
-
-            const aside = document.querySelector('.clientOrders');
-            aside.innerHTML = '';
-            const h1Order = document.createElement('h1')
-            h1Order.innerHTML = 'Order'
-            aside.appendChild(h1Order)
-            
-            let idsUsed = new Set()
-
-            //Maybe here we can use Promise All to get all products at the same time
-            for (const productId of productsIds) {
-                if (!idsUsed.has(productId)) {
-                    idsUsed.add(productId)
-                    const responseProduct = await fetch('api_getProduct.php?id_product=' + productId);
-                    const product = await responseProduct.json();
-                    
-                    const productDiv = document.createElement('div')
-                    const productName = document.createElement('strong')
-                    const productPrice = document.createElement('em')
-                    const productQuantity = document.createElement('em')
-                    const productRemove = document.createElement('button')
-
-                    const price = (product.price * (1 - (product.discount/100)));
-                    productPrice.innerHTML = (price.toFixed(2))  + ' € ';
-                    productName.innerHTML = product.name
-                    productQuantity.innerHTML = 1;
-                    productQuantity.id = "productQuantity" + productId;
-                    productRemove.innerHTML = 'Remove'
-                    productRemove.value = product.id
-                    productRemove.addEventListener('click', async function() {
-                        await fetch('api_removeSessionProduct.php?id_product=' + this.value);
-                        const productDivToRemove = document.getElementById('productDiv' + productId);
-                        if (productDivToRemove) {
-                            productDivToRemove.remove();
-                        }
-                    })
-                    productRemove.classList.add('productOrderRemoveButton')
-
-                    productDiv.classList.add('productInOrders')
-                    productDiv.id = 'productDiv' + productId
-                    productDiv.appendChild(productName)
-                    productDiv.appendChild(productPrice)
-                    productDiv.innerHTML += " Quantity: "
-                    productDiv.appendChild(productQuantity)
-                    productDiv.appendChild(productRemove)
-                    aside.appendChild(productDiv)
-                
-                } else {
-                    const productQuantityId = document.querySelector("#productQuantity" + productId)
-                    if (productQuantityId) {
-                        productQuantityId.innerHTML = parseInt(productQuantityId.innerHTML) + 1;
-                    }
-                        
-                }
-
-            }
-            
-
-            
-            /*
-            const sessionResponse = await fetch('api_getSession.php')
-            const sessionArray = await sessionResponse.json();
-            const session = sessionArray[0];
-            console.log(session['id_restaurant']);
-            */          
-            
-        })
+    addProductsToOrderButtons.forEach( function(addProductsToOrderButton) {
+        addProductsToOrderButton.addEventListener('click', function() {
+            showProductInOrder(addProductsToOrderButton.value, true)
+        })         
     });
 }
 
 
 
 
+     /*
+            const sessionResponse = await fetch('api_getSession.php')
+            const sessionArray = await sessionResponse.json();
+            const session = sessionArray[0];
+            console.log(session['id_restaurant']);
+            */        
