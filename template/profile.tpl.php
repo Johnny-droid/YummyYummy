813345dfecfbf3751ea9ed23a4ee8e8d67291423
José Araújo <ze.b.araujo@outlook.com>
2022-05-17 21:58:37 +0000

    adding profile pic and some tuneups in profile page

--- template/profile.tpl.php
+++ template/profile.tpl.php
@@ -1,5 +1,10 @@
 <?php function output_profile(User $user) { ?>
+    <div class="pic">
+        <img src="../images/Artworks/Profile Pictures/profile1.png" alt="Avatar" class="avatar">
+    </div>
+
     <div class="profiling">
+        
         <h1>@<?= $user->name ?></h1><br>
         <strong>Age: <?= $user->age ?></strong><br>
         <strong>Address: <?= $user->address ?></strong><br>
