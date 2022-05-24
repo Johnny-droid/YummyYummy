ce4a293241355c99ed33f7143cbdaacae004e3d7
Johnny-droid <joaoricardoralves@gmail.com>
2022-05-24 20:22:11 +0100

    Small changes to css in profile

--- template/profile.tpl.php
+++ template/profile.tpl.php
@@ -1,31 +1,34 @@
 <?php function output_profile(User $user, $favouriteRestaurants) { ?>
-    <div class="pic">
-        <label class="-label" for="file">
-            <span>Change Image</span>
-        </label>
-        <input id="file" type="file" onchange="loadFile(event)"/>
-        <img src="../images/Artworks/Profile Pictures/profile1.png" alt="Avatar" class="avatar">
-    </div>
+    <div class="profile">
+        <div class="pic">
+            <label class="-label" for="file">
+                <span>Change Image</span>
+            </label>
+            <input id="file" type="file" onchange="loadFile(event)" />
+            <img src="../images/Artworks/Profile Pictures/profile1.png" alt="Avatar" class="avatar">
+        </div>
 
-    <div class="profiling">
-        
-        <h1>@<?= $user->name ?></h1><br>
-        <strong>Age: <?= $user->age ?></strong><br>
-        <strong>Address: <?= $user->address ?></strong><br>
-        <strong>Phone Number: <?= $user->phoneNumber ?></strong><br>
-        <strong>Email: <?= $user->email ?></strong><br>
-        <strong>Bio: <?= $user->bio ?></strong><br>
-        <div class="favourites">
-            <?php
+        <div class="profiling">
+
+            <h1>@<?= $user->name ?></h1>
+            <div class="itemProfile">Age: <?= $user->age ?></div>
+            <div class="itemProfile">Address: <?= $user->address ?></div>
+            <div class="itemProfile">Phone Number: <?= $user->phoneNumber ?></div>
+            <div class="itemProfile">Email: <?= $user->email ?></div>
+            <div class="itemProfile">Bio: <?= $user->bio ?></div>
+            <div class="favourites">
+                <?php
                 if ($favouriteRestaurants) { ?>
                     <h3 id="headingers">Favourite Restaurants </h3>
-            <?php        foreach($favouriteRestaurants as $restaurant) { ?>
-                            <strong><?=$restaurant->name?></strong> <br>
-            <?php }
+                    <?php foreach ($favouriteRestaurants as $restaurant) { ?>
+                        <div class="itemProfile"><?= $restaurant->name ?></div>
+                <?php }
                 } ?>
+            </div>
         </div>
+
     </div>
-    
-    
-        
+
+
+
 <?php } ?>
