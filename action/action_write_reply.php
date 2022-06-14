4f8eba981b6b18b6c8472856bf3c60406b9ab67b
Johnny-droid <joaoricardoralves@gmail.com>
2022-06-14 15:57:29 +0100

    Added filters and encodes for reviews and replies

--- action/action_write_reply.php
+++ action/action_write_reply.php
@@ -7,27 +7,27 @@
     require_once( __DIR__ . '/../database/review.class.php');
 
     $db = getDatabaseConnection();
-    $id_review = intval($_POST['id_review']);
-    $reply = $_POST['reply'];
+    $id_review = intval(preg_replace('/\D/', '', $_POST['id_review']));
+    $reply = preg_replace('/[^a-zA-Z!?.@,\s]/', '', $_POST['reply']);
 
     if (!isset($_SESSION['id']) || !isset($_SESSION['id_restaurant']) || !isset($_SESSION['ids_restaurants_owned']) || $_SESSION['type'] !== 'O') {
         $url = parse_url($_SERVER['HTTP_REFERER']);
         parse_str($url['query'], $query);
-        header('Location: ../pages/restaurant.php?id=' . $query['id'] . '&error_reply=1'); //add error
+        header('Location: ../pages/restaurant.php?id=' . preg_replace('/\D/', '', $query['id']) . '&error_reply=1'); //add error
         exit();
     }
 
     if ($id_review <= 0 || $reply === '') {
         $url = parse_url($_SERVER['HTTP_REFERER']);
         parse_str($url['query'], $query);
-        header('Location: ../pages/restaurant.php?id=' . $query['id'] . '&error_reply=2'); //add error
+        header('Location: ../pages/restaurant.php?id=' . preg_replace('/\D/', '', $query['id']) . '&error_reply=2'); //add error
         exit();
     }
 
     if (!isset($_SESSION['ids_restaurants_owned'][$_SESSION['id_restaurant']])) {
         $url = parse_url($_SERVER['HTTP_REFERER']);
         parse_str($url['query'], $query);
-        header('Location: ../pages/restaurant.php?id=' . $query['id'] . '&error_reply=3'); //add error
+        header('Location: ../pages/restaurant.php?id=' . preg_replace('/\D/', '', $query['id']) . '&error_reply=3'); //add error
         exit();
     }
 
@@ -37,7 +37,7 @@
     if (!$success) {
         $url = parse_url($_SERVER['HTTP_REFERER']);
         parse_str($url['query'], $query);
-        header('Location: ../pages/restaurant.php?id=' . $query['id'] . '&error_reply=4'); //add error
+        header('Location: ../pages/restaurant.php?id=' . preg_replace('/\D/', '', $query['id']) . '&error_reply=4'); //add error
         exit();
     }
     
--- action/action_write_review.php
+++ action/action_write_review.php
@@ -7,21 +7,21 @@
     require_once( __DIR__ . '/../database/review.class.php');
 
     $db = getDatabaseConnection();
-    $rating = intval($_POST['rating']);
-    $price = intval($_POST['price']);
-    $comment = $_POST['comment'];
+    $rating = intval(preg_replace('/\D/', '', $_POST['rating']));
+    $price = intval(preg_replace('/\D/', '', $_POST['price']));
+    $comment = preg_replace('/[^a-zA-Z!?.@,\s]/', '', $_POST['comment']);
 
     if (!isset($_SESSION['id']) || !isset($_SESSION['id_restaurant']) || $_SESSION['type'] !== 'C') {
         $url = parse_url($_SERVER['HTTP_REFERER']);
         parse_str($url['query'], $query);
-        header('Location: ../pages/restaurant.php?id=' . $query['id'] . '&error_review=1'); //add error
+        header('Location: ../pages/restaurant.php?id=' . preg_replace('/\D/', '', $query['id']) . '&error_review=1'); //add error
         exit();
     }
 
     if ($rating < 1 || $rating > 5 || $price < 1 || $price > 5) {
         $url = parse_url($_SERVER['HTTP_REFERER']);
         parse_str($url['query'], $query);
-        header('Location: ../pages/restaurant.php?id=' . $query['id'] . '&error_review=2'); //add error
+        header('Location: ../pages/restaurant.php?id=' . preg_replace('/\D/', '', $query['id']) . '&error_review=2'); //add error
         exit();
     }
 
@@ -30,7 +30,7 @@
     if (!$success) {
         $url = parse_url($_SERVER['HTTP_REFERER']);
         parse_str($url['query'], $query);
-        header('Location: ../pages/restaurant.php?id=' . $query['id'] . '&error_review=3'); //add error
+        header('Location: ../pages/restaurant.php?id=' . preg_replace('/\D/', '', $query['id']) . '&error_review=3'); //add error
         exit();
     }
     
